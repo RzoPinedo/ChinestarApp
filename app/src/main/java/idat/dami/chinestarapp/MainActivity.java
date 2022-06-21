626c1bc72233048dc77256410be891442d35e00d
YRSON <yersonsifuentesc20@gmail.com>
2022-06-20 22:08:12 -0500

    NavigationBottom

--- app/src/main/java/idat/dami/chinestarapp/MainActivity.java
+++ app/src/main/java/idat/dami/chinestarapp/MainActivity.java
@@ -1,8 +1,14 @@
 package idat.dami.chinestarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
