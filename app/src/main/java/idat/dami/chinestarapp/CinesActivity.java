0b820ce4472eede479565f36b7e15fb81cd40e06
raul-cuenca <raul.cuenca.sarrin@gmail.com>
2022-06-20 22:27:58 -0500

    cines

--- app/src/main/java/idat/dami/chinestarapp/CinesActivity.java
+++ app/src/main/java/idat/dami/chinestarapp/CinesActivity.java
@@ -10,5 +10,7 @@ public class CinesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cines);
+
+        getSupportActionBar().hide();
     }
 }
