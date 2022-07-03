7c4a3cd7d05e1a251ae1e27d131aa918dc74da11
raul-cuenca <raul.cuenca.sarrin@gmail.com>
2022-07-03 01:58:23 -0500

    corrigiendo registro.xml

--- app/src/main/java/idat/dami/chinestarapp/MainActivity.java
+++ app/src/main/java/idat/dami/chinestarapp/MainActivity.java
@@ -11,14 +11,31 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floating_action_button)
+
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent objInicio = new Intent(this, RegistroActivity.class);
+                startActivity(objInicio);
+            }
+        });
     }
 
 }
